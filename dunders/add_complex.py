f43a5be5fc7f32b279b38a44d688b670916eebe8
Douglas Naphas <douglasnaphas@gmail.com>
2021-09-02 00:47:50 -0400

    Show how to override plus
    https://www.geeksforgeeks.org/operator-overloading-in-python/

---
+++ dunders/add_complex.py
@@ -0,0 +1,20 @@
+# Python Program to perform addition
+# of two complex numbers using binary
+# + operator overloading.
+
+class complex:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    # adding two objects
+    def __add__(self, other):
+        return complex(self.a + other.a, self.b + other.b)
+
+Ob1 = complex(1, 2)
+Ob2 = complex(2, 3)
+Ob3 = Ob1 + Ob2
+print(Ob3)
+print(Ob3.a)
+print(Ob3.b)
+
